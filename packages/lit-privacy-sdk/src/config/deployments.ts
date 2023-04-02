1f70d89af48381deb9a0265c123b0351db0ae33f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-04-03 00:20:14 +0530

    chore: added deployed address of PVR on mumbai network

--- packages/lit-privacy-sdk/src/config/deployments.ts
+++ packages/lit-privacy-sdk/src/config/deployments.ts
@@ -2,7 +2,7 @@ import { Deployments } from "../types/Deployments";
 
 const deployments: Deployments = {
     goerli: "",
-    mumbai: "",
+    mumbai: "0x9694B1806fc6b2905b31FA5A8bc1a731E4CcC973",
     matic: "",
     ethereum: "",
 }
