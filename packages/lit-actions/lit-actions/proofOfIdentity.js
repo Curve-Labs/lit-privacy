82548633e30bab0122ebc2191d099089f632c939
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-04-17 23:18:19 +0530

    chore: updated lit action to use EIP191 signing and added logs to lit actions

--- packages/lit-actions/lit-actions/proofOfIdentity.js
+++ packages/lit-actions/lit-actions/proofOfIdentity.js
@@ -4,9 +4,8 @@ export default `
     const address = await ethers.utils.verifyMessage(message, signature);
     const parsedBlockNumber = ethers.utils.hexZeroPad(ethers.utils.hexlify(blockNumber), 32);
     const dataToBeSigned = ethers.utils.hexlify(ethers.utils.concat([address, parsedBlockNumber, publicSignal]));
-    const toSign = Array.from(ethers.utils.arrayify(ethers.utils.hashMessage(dataToBeSigned)))
-
-    const sigShare = await LitActions.signEcdsa({ toSign: toSign, publicKey: publicKey, sigName: "sig1" });
+    console.log({message, signature, blockNumber, publicSignal, address, parsedBlockNumber, dataToBeSigned});
+    const sigShare = await LitActions.ethPersonalSignMessageEcdsa({ message: dataToBeSigned, publicKey: publicKey, sigName: "sig1" });
   };
 
   generateProofOfIdentity();
